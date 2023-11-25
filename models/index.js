f8244c76bc9bacad39f138008ae90c6184149d18
Hyunjun Angelo Jeon <hajccts417@gmail.com>
2023-11-25 13:50:32 -0600

    Minor change to One-To-Many association

--- models/index.js
+++ models/index.js
@@ -5,9 +5,7 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
 Category.hasMany(Product, {
